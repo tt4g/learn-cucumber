c6ca605e42b321b1c348473911863d11d8a226f0
tt4g <mainlinett@gmail.com>
2021-11-18 13:50:31 +0900

    Use cucumber-java instead cucumber-java8

--- learn-cucumber/src/test/java/com/github/tt4g/lean/cucumber/IsItFridaySteps.java
+++ learn-cucumber/src/test/java/com/github/tt4g/lean/cucumber/IsItFridaySteps.java
@@ -1,31 +1,30 @@
 package com.github.tt4g.lean.cucumber;
 
-import io.cucumber.java8.En;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class IsItFridaySteps implements En {
+public class IsItFridaySteps {
 
     private String today;
 
     private String actualAnswer;
 
-    public IsItFridaySteps() {
-        Given("today is {string}", (String today) -> {
-            this.today = today;
-        });
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
 
-        Given("today is Friday", () -> {
-            this.today = "Friday";
-        });
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        this.actualAnswer = IsItFriday.isItFriday(this.today);
+    }
 
-        When("I ask whether it's Friday yet", () -> {
-            this.actualAnswer = IsItFriday.isItFriday(this.today);
-        });
-
-        Then("I should be told {string}", (String expectedAnswer) -> {
-            assertThat(expectedAnswer).isEqualTo(this.actualAnswer);
-        });
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertThat(expectedAnswer).isEqualTo(this.actualAnswer);
     }
 
 }
